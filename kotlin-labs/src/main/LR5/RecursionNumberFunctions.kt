77624ac0f82c4e64a7e833e35ae15171b42bca36
korsNaike <kirillgrinev2003@mail.ru>
2024-05-18 02:54:57 +0300

    ЛР5. Задание 5. Написание тестов и исправления

--- kotlin-labs/src/main/LR5/RecursionNumberFunctions.kt
+++ kotlin-labs/src/main/LR5/RecursionNumberFunctions.kt
@@ -1,14 +1,16 @@
+import kotlin.math.abs;
+
 class RecursionNumberFunctions {
 
     /**
      * Нахождение минимальной цифры числа с помощью рекурсии вверх
      */
     fun findMinDigit(number: Int): Int {
-        return if (number < 10) {
+        return if (abs(number) < 10) {
             number
         } else {
-            val digit = number % 10
-            val minInRest = findMinDigit(number / 10)
+            val digit = abs(number % 10)
+            val minInRest = abs(findMinDigit(number / 10))
             if (digit < minInRest) digit else minInRest
         }
     }
@@ -20,11 +22,11 @@ class RecursionNumberFunctions {
         return if (number == 0) {
             minDigit ?: number
         } else {
-            val digit = number % 10
+            val digit = abs(number % 10)
 
             var newMinDigit = digit
             if (minDigit != null) {
-                newMinDigit = if (digit < minDigit) digit else minDigit
+                newMinDigit = abs(if (digit < minDigit) digit else minDigit)
             }
 
             findMinDigitTailRec(number / 10, newMinDigit)
@@ -37,10 +39,10 @@ class RecursionNumberFunctions {
     fun productOfNonDivisibleBy5(number: Int): Int {
         return when {
             number == 0 -> 0 // Базовый случай: если число равно 0, то произведение равно 0
-            number < 10 -> if (number % 5 != 0) number else 0 // Если число делится на 5, то произведение равно 0
+            abs(number) < 10 -> if (number % 5 != 0) abs(number) else 0 // Если число делится на 5, то произведение равно 0
             else -> {
-                val digit = number % 10
-                val productInRest = productOfNonDivisibleBy5(number / 10)
+                val digit = abs(number % 10)
+                val productInRest = abs(productOfNonDivisibleBy5(number / 10))
                 if (digit % 5 != 0) digit * productInRest else productInRest
             }
         }
@@ -51,11 +53,12 @@ class RecursionNumberFunctions {
      */
     tailrec fun productOfNonDivisibleBy5Tail(number: Int, product: Int = 0): Int {
         return if (number == 0) {
-            product
+            abs(product)
         } else {
             val digit = number % 10
-            val newProduct =
+            val newProduct = abs(
                 if (digit % 5 != 0 && product == 0) digit else if (digit % 5 != 0) product * digit else product
+            )
             productOfNonDivisibleBy5Tail(number / 10, newProduct)
         }
     }
@@ -63,7 +66,7 @@ class RecursionNumberFunctions {
     /**
      * Нахождение НОД двух чисел с помощью хвостовой рекурсии
      */
-    tailrec fun nod(a: Int, b: Int): Int = if (b == 0) a else nod(b, a % b)
+    tailrec fun nod(a: Int, b: Int): Int = if (b == 0) abs(a) else nod(b, a % b)
 
     /**
      * Функция высших порядков для нахождения максимума среди чисел в списке по переданной функции
--- kotlin-labs/src/test/LR5/RecursionNumberTest.kt
+++ kotlin-labs/src/test/LR5/RecursionNumberTest.kt
@@ -10,6 +10,11 @@ class RecursionNumberTest {
         assertEquals(0, recursionFunc.findMinDigit(0))
         assertEquals(1, recursionFunc.findMinDigit(54321))
         assertEquals(3, recursionFunc.findMinDigit(999939939))
+        assertEquals(1, recursionFunc.findMinDigit(12345))
+        assertEquals(2, recursionFunc.findMinDigit(-23456))
+        assertEquals(5, recursionFunc.findMinDigit(5))
+        assertEquals(0, recursionFunc.findMinDigit(1000))
+        assertEquals(1, recursionFunc.findMinDigit(1234))
     }
 
     @Test
@@ -19,6 +24,11 @@ class RecursionNumberTest {
         assertEquals(0, recursionFunc.findMinDigitTailRec(0))
         assertEquals(1, recursionFunc.findMinDigitTailRec(54321))
         assertEquals(3, recursionFunc.findMinDigitTailRec(999939939))
+        assertEquals(1, recursionFunc.findMinDigit(12345))
+        assertEquals(2, recursionFunc.findMinDigit(-23456))
+        assertEquals(5, recursionFunc.findMinDigit(5))
+        assertEquals(0, recursionFunc.findMinDigit(1000))
+        assertEquals(1, recursionFunc.findMinDigit(1234))
     }
 
     @Test
@@ -29,6 +39,7 @@ class RecursionNumberTest {
         assertEquals(0, recursionFunc.productOfNonDivisibleBy5(555))
         assertEquals(0, recursionFunc.productOfNonDivisibleBy5(0))
         assertEquals(1, recursionFunc.productOfNonDivisibleBy5(1))
+        assertEquals(32, recursionFunc.productOfNonDivisibleBy5(-854))
     }
 
     @Test
@@ -39,6 +50,7 @@ class RecursionNumberTest {
         assertEquals(0, recursionFunc.productOfNonDivisibleBy5Tail(555))
         assertEquals(0, recursionFunc.productOfNonDivisibleBy5Tail(0))
         assertEquals(1, recursionFunc.productOfNonDivisibleBy5Tail(1))
+        assertEquals(32, recursionFunc.productOfNonDivisibleBy5Tail(-854))
     }
 
     @Test
@@ -47,6 +59,10 @@ class RecursionNumberTest {
         assertEquals(12, recursionFunc.nod(24, 36))
         assertEquals(5, recursionFunc.nod(15, 10))
         assertEquals(7, recursionFunc.nod(14, 21))
+        assertEquals(2, recursionFunc.nod(6, 8))
+        assertEquals(5, recursionFunc.nod(5, 0))
+        assertEquals(3, recursionFunc.nod(-9, -6))
+        assertEquals(1, recursionFunc.nod(7, 4))
     }
 
     @Test
