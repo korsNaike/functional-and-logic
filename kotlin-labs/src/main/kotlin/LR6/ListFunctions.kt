95bc0ba2e08cde2f288121db7ece499335c3e860
korsNaike <kirillgrinev2003@mail.ru>
2024-05-19 19:51:40 +0300

    ЛР6. Задание 3. Реализованы функции работы с коллекциями на Kotlin

--- kotlin-labs/src/main/kotlin/LR6/ListFunctions.kt
+++ kotlin-labs/src/main/kotlin/LR6/ListFunctions.kt
@@ -1,6 +1,7 @@
 package LR6
 
 import LR5.RecursionNumberFunctions
+import kotlin.math.max
 
 class ListFunctions {
 
@@ -51,4 +52,69 @@ class ListFunctions {
      * Количество делителей
      */
     fun countDivisors(n: Int): Int = (1..n).count { n % it == 0 }
+
+    /**
+     * Определить, является ли элемент по индексу глобальным максимумом
+     */
+    fun indexIsGlobalMin(list: List<Int>, index: Int): Boolean = list[index] == list.minOf { it }
+
+    /**
+     * Посчитать количество совпадающих по значению элементов
+     */
+    fun countOfEqualsElements(list1: List<Int>, list2: List<Int>): Int {
+        val set1 = list1.toSet()
+        val set2 = list2.toSet()
+
+        return set1.count { it in set2 }
+    }
+
+    /**
+     * Найти количество минимумов в интервале
+     */
+    fun findCountMinInInterval(list: List<Int>, interval: Pair<Int, Int>): Int {
+        val filterList = list.filter { it >= interval.first && it <= interval.second }
+        println(filterList)
+        val minimum = filterList.minOf { it }
+
+        return filterList.count { it == minimum }
+    }
+
+    /**
+     * Получить список элементов, которые находятся между первым и вторым максимумом
+     */
+    fun getBetweenMaxList(list: List<Int>): List<Int> {
+        val maxTo = list.indexOf(list.maxOf { it })
+        val listWithoutFirstMax = list.filterIndexed { index, i -> index != maxTo }
+        val maxFromPre = listWithoutFirstMax.indexOf(listWithoutFirstMax.maxOf { it })
+        val maxFrom = if (maxFromPre > maxTo) maxFromPre + 1 else maxFromPre
+
+        return if (maxTo > maxFrom) {
+            list.filterIndexed { index, _ ->  index > maxFrom && index < maxTo}
+        } else {
+            list.filterIndexed { index, _ ->  index < maxFrom && index > maxTo}
+        }
+    }
+
+    /**
+     * Проверить наличие максимального элемента в интервале
+     */
+    fun isMaxInInterval(list: List<Int>, interval: Pair<Int, Int>): Boolean {
+        val filterList = list.filter { it >= interval.first && it <= interval.second }
+        val maximum = list.maxOf { it }
+
+        return filterList.contains(maximum)
+    }
+
+    /**
+     * Посчитать количество локальных максимумов
+     */
+    fun countLocalMaxima(list: List<Int>): Int {
+        return list.indices.count { index ->
+            val curr = list[index]
+            val prev = if (index == 0) Int.MIN_VALUE else list[index - 1]
+            val next = if (index == list.lastIndex) Int.MIN_VALUE else list[index + 1]
+            curr > prev && curr > next
+        }
+    }
+
 }
--- kotlin-labs/src/test/test/LR6/ListFunctionTest.kt
+++ kotlin-labs/src/test/test/LR6/ListFunctionTest.kt
@@ -28,4 +28,70 @@ class ListFunctionTest {
             listOf(8, 16, 11)
         ))
     }
+
+    @Test
+    fun testElementAtIndexIsMinimum() {
+        val listFunctions = ListFunctions()
+        val testList = listOf(5, 3, 7, 1, 9)
+        val index = 3
+        val result = listFunctions.indexIsGlobalMin(testList, index)
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun testCountOfEqualsElements() {
+        val listFunctions = ListFunctions()
+        assertEquals(4, listFunctions.countOfEqualsElements(listOf(1, 2, 3, 4), listOf(1, 2, 3, 4)))
+        assertEquals(0, listFunctions.countOfEqualsElements(listOf(5, 6, 7, 8), listOf(1, 2, 3, 4)))
+        assertEquals(0, listFunctions.countOfEqualsElements(listOf(), listOf(1, 2, 3, 4)))
+        assertEquals(1, listFunctions.countOfEqualsElements(listOf(1, 2), listOf(2)))
+    }
+
+    @Test
+    fun testFindCountMinInInterval() {
+        val listFunctions = ListFunctions()
+        assertEquals(3, listFunctions.findCountMinInInterval(
+            listOf(1, 2, 3, 4, 4, 4, 5, 6, 7),
+            Pair(4, 7))
+        )
+        assertEquals(1, listFunctions.findCountMinInInterval(
+            listOf(1, 2, 3, 4, 4, 4, 5, 6, 7),
+            Pair(1, 5))
+        )
+    }
+
+    @Test
+    fun testGetBetweenMaxList() {
+        val listFunctions = ListFunctions()
+        assertEquals(
+            listOf(4, 5, 6),
+            listFunctions.getBetweenMaxList(listOf(1, 2, 3, 4, 7, 4, 5, 6, 7))
+        )
+        assertEquals(
+            listOf(4, 3, 4, 5, 1),
+            listFunctions.getBetweenMaxList(listOf(1, 2, 8, 4, 3, 4, 5, 1, 7))
+        )
+    }
+
+    @Test
+    fun testIsMaxInInterval() {
+        val listFunctions = ListFunctions()
+        assertEquals(
+            false,
+            listFunctions.isMaxInInterval(listOf(1, 3, 4, 5, 7, 8), Pair(1, 5))
+        )
+
+        assertEquals(
+            true,
+            listFunctions.isMaxInInterval(listOf(1, 3, 4, 5, 7, 8), Pair(5, 10))
+        )
+    }
+
+    @Test
+    fun testCountLocalMaxima() {
+        val listFunctions = ListFunctions()
+        assertEquals(3, listFunctions.countLocalMaxima(listOf(1, 3, 2, 5, 4, 6, 2)))
+        assertEquals(3, listFunctions.countLocalMaxima(listOf(3, 2, 5, 4, 6, 2)))
+        assertEquals(3, listFunctions.countLocalMaxima(listOf(3, 2, 5, 4, 2, 6)))
+    }
 }
